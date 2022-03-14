159f854c406dfdbca4f1a8b158c52f0d5fad669b
cosmic <yafetyohaness@gmail.com>
2022-03-14 13:45:04 +0300

    Add normal button to change color to original

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const reset=document.querySelector('#reset')
 const rainbow=document.querySelector('#rainbow')
 const normalColor=document.querySelector("#normal")
 let Color='black';
+let colorRain=true;
 
 
 addDiv(divNumber);
@@ -35,6 +36,8 @@ function colorChange(){
     const squares=document.querySelectorAll('.square');
 squares.forEach(square => {
               square.addEventListener('mouseover',()=>{
+                if(!colorRain){Color=`rgb(${randomRgb()},${randomRgb()},${randomRgb()})`;}
+                else Color='black';
                   square.style.backgroundColor=Color;
               })    
 });
@@ -45,7 +48,8 @@ function resetColor(){
     reset.addEventListener('click',()=>{
         const squares=document.querySelectorAll('.square');
         squares.forEach(square => {
-                          square.style.backgroundColor="grey";
+             square.style.backgroundColor="grey";  
+             
                       })    
         });
    
@@ -56,12 +60,10 @@ function randomRgb(){
 }
 
 function colorful(){
-    rainbow.addEventListener('click',()=>{
-        Color=`rgb(${randomRgb()},${randomRgb()},${randomRgb()})`;
-    });
+    rainbow.addEventListener('click',()=>colorRain=false);
 }
 function normalButton(){
     normalColor.addEventListener('click',()=>{
-        Color='black';
+        colorRain=true;
     })
 }
